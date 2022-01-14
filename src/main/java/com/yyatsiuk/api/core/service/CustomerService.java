8a114ad60fa68b95f5df1542c3d5ba1e3370af40
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-01-14 10:55:54 +0200

    Define customer service interface

--- src/main/java/com/yyatsiuk/api/core/service/CustomerService.java
+++ src/main/java/com/yyatsiuk/api/core/service/CustomerService.java
@@ -1,5 +1,17 @@
 package com.yyatsiuk.api.core.service;
 
+import com.yyatsiuk.api.core.dto.CustomerDto;
+
+import java.util.List;
+
 public interface CustomerService {
+
+    Long save(CustomerDto customerDto);
+
+    CustomerDto update(CustomerDto customerDto);
+
+    CustomerDto findById(Long id);
+
+    List<CustomerDto> getAll();
 
 }
