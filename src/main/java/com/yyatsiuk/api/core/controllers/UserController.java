cb03b7be13ba554b2db6e4054c49386896d9e3b8
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-02-03 20:53:17 +0200

    Update UserController

--- src/main/java/com/yyatsiuk/api/core/controllers/UserController.java
+++ src/main/java/com/yyatsiuk/api/core/controllers/UserController.java
@@ -1,16 +1,21 @@
 package com.yyatsiuk.api.core.controllers;
 
 import com.yyatsiuk.api.core.models.dto.UserDto;
+import com.yyatsiuk.api.core.models.mappers.UserMapper;
 import com.yyatsiuk.api.core.models.request.UserCreateRequest;
+import com.yyatsiuk.api.core.models.request.UserUpdateRequest;
 import com.yyatsiuk.api.core.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/users")
@@ -18,19 +23,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final UserMapper userMapper;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService,
+                          UserMapper userMapper) {
         this.userService = userService;
+        this.userMapper = userMapper;
     }
 
     @PostMapping
-    public ResponseEntity<Void> createUser(@RequestBody UserCreateRequest payload) {
+    public ResponseEntity<Void> createUser(@RequestBody @Valid UserCreateRequest payload) {
+        UserDto userDto = userMapper.fromCreateRequestToDto(payload);
+
+        userService.save(userDto);
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("${id}")
+    @PutMapping
+    public ResponseEntity<UserDto> updateUser(@RequestBody @Valid UserUpdateRequest payload) {
+        UserDto userDto = userMapper.fromUpdateRequestToDto(payload);
+
+        return ResponseEntity.ok(userService.update(userDto));
+    }
+
+    @GetMapping("/{id}")
     public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
-        return ResponseEntity.ok(new UserDto());
+        return ResponseEntity.ok(userService.findById(id));
     }
 
 }
--- src/main/java/com/yyatsiuk/api/core/models/mappers/UserMapper.java
+++ src/main/java/com/yyatsiuk/api/core/models/mappers/UserMapper.java
@@ -3,6 +3,7 @@ package com.yyatsiuk.api.core.models.mappers;
 import com.yyatsiuk.api.core.models.dto.UserDto;
 import com.yyatsiuk.api.core.models.entities.User;
 import com.yyatsiuk.api.core.models.request.UserCreateRequest;
+import com.yyatsiuk.api.core.models.request.UserUpdateRequest;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
@@ -13,4 +14,7 @@ public interface UserMapper {
     User fromDtoToEntity(UserDto dto);
 
     UserDto fromCreateRequestToDto(UserCreateRequest request);
+
+    UserDto fromUpdateRequestToDto(UserUpdateRequest request);
+
 }
